30a4a2892a287a5b0451f96e1a9a8946b8fa44d6
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2024-09-29 23:50:58 +0530

    adds cpu service

---
+++ internal/service/cpu/cpu.go
@@ -0,0 +1,51 @@
+package cpu
+
+import (
+	"context"
+	"cquest/internal/models"
+)
+
+func (svc *CPUService) GetAllCPUs(ctx context.Context) ([]models.CPU, error) {
+	cpus, err := svc.repo.GetAllCPUs(ctx)
+	result := make([]models.CPU, len(cpus))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (svc *CPUService) AddCPU(ctx context.Context, cpu models.CPU) error {
+	err := svc.repo.AddCPU(ctx, cpu)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (svc *CPUService) UpdateCPU(ctx context.Context, cpu models.CPU) error {
+	err := svc.repo.UpdateCPU(ctx, cpu)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (svc *CPUService) DeleteCPUByID(ctx context.Context, id int) error {
+	err := svc.repo.DeleteCPUByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (svc *CPUService) GetCPUByID(ctx context.Context, id int) (models.CPU, error) {
+	var cpu models.CPU
+	cpu, err := svc.repo.GetCPUByID(ctx, id)
+	if err != nil {
+		return cpu, err
+	}
+	return cpu, nil
+}
