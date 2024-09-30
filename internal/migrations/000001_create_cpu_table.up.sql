1b1e0c78d429a1df211654ea83bbed5dd975c6f1
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2024-09-30 12:50:43 +0530

    adds cpu table migrations

--- internal/migrations/000001_create_cpu_table.up.sql
+++ internal/migrations/000001_create_cpu_table.up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE cpu
 (
-    id          SERIAL PRIMARY KEY,
-    Model       TEXT,
-    Cores       INT,
-    Threads     INT,
-    Cache       INT,
-    BaseClock   INT,
-    MaxClock    INT,
-    Rank        INT,
-    ReleaseDate TIMESTAMPTZ,
-    inserted_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id           SERIAL PRIMARY KEY,
+    model        TEXT,
+    cores        INT,
+    threads      INT,
+    cache        INT,
+    base_clock   INT,
+    max_clock    INT,
+    rank         INT,
+    release_date TIMESTAMPTZ,
+    inserted_at  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
