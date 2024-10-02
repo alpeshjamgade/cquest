3bc1ab4d0ef7b70aa621fc60577664612fb0b726
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2024-10-02 18:05:26 +0530

    adds gpu service

---
+++ internal/service/gpu/gpu.go
@@ -0,0 +1,50 @@
+package gpu
+
+import (
+	"context"
+	"cquest/internal/models"
+)
+
+func (svc GPUService) GetAllGPUs(ctx context.Context) ([]models.GPU, error) {
+	gpus, err := svc.repo.GetAllGPUs(ctx)
+	if err != nil {
+		return gpus, err
+	}
+
+	return gpus, nil
+}
+
+func (svc GPUService) AddGPU(ctx context.Context, gpu models.GPU) error {
+	err := svc.repo.AddGPU(ctx, gpu)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (svc GPUService) UpdateGPU(ctx context.Context, gpu models.GPU) error {
+	err := svc.repo.UpdateGPU(ctx, gpu)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (svc GPUService) DeleteGPU(ctx context.Context, id int) error {
+	err := svc.repo.DeleteGPU(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (svc GPUService) GetGPUByID(ctx context.Context, id int) (models.GPU, error) {
+	gpu, err := svc.repo.GetGPUByID(ctx, id)
+	if err != nil {
+		return gpu, err
+	}
+
+	return gpu, nil
+}
---
+++ internal/service/gpu/service.go
@@ -0,0 +1,23 @@
+package gpu
+
+import (
+	"context"
+	"cquest/internal/models"
+	"cquest/internal/repo/gpu"
+)
+
+type IGPUService interface {
+	GetAllGPUs(ctx context.Context) ([]models.GPU, error)
+	AddGPU(ctx context.Context, gpu models.GPU) error
+	UpdateGPU(ctx context.Context, gpu models.GPU) error
+	DeleteGPUByID(ctx context.Context, id int) error
+	GetGPUByID(ctx context.Context, id int) (models.GPU, error)
+}
+
+type GPUService struct {
+	repo gpu.IGPURepo
+}
+
+func NewGPUService(repo gpu.IGPURepo) *GPUService {
+	return &GPUService{repo: repo}
+}
